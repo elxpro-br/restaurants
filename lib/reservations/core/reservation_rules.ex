de4ad9a72e8cada9b0caececf4226b66d077ffb6
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-03-03 19:10:41 -0300

    add

--- lib/reservations/core/reservation_rules.ex
+++ lib/reservations/core/reservation_rules.ex
@@ -6,4 +6,17 @@ defmodule Reservations.Core.ReservationRules do
   def create_restaurant(name, num_tables) do
     Restaurant.new(name, num_tables)
   end
+
+  def make_reservation(%{tables: tables} = restaurant, {name, seats}) do
+    new_table = %{
+      food_plate: "Steak",
+      forks: seats,
+      knives: seats,
+      name: name,
+      seats: seats,
+      spoons: seats
+    }
+
+    %{restaurant | tables: tables ++ [new_table]}
+  end
 end
