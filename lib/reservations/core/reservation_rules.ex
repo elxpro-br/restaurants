4a6e7f6f1e746937441fd1ca2ccef5941b3173aa
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-03-03 18:05:28 -0300

    learning typespecs

--- lib/reservations/core/reservation_rules.ex
+++ lib/reservations/core/reservation_rules.ex
@@ -1,6 +1,8 @@
 defmodule Reservations.Core.ReservationRules do
   alias Reservations.Data.Restaurant
+  @type restaurant :: Restaurant.t()
 
+  @spec create_restaurant(String.t(), integer()) :: restaurant()
   def create_restaurant(name, num_tables) do
     Restaurant.new(name, num_tables)
   end
--- lib/reservations/reservations.ex
+++ lib/reservations/reservations.ex
@@ -1,5 +1,7 @@
 defmodule Reservations do
+  alias Reservations.Type
   alias Reservations.Core.ReservationRules
 
+  @spec create_restaurant(String.t(), integer()) :: Type.restaurant()
   defdelegate create_restaurant(name, num_tables), to: ReservationRules
 end
