77d75ebe715ac5a41fa7b3583ff65b36d0fd132d
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-03-09 15:49:48 -0300

    pensando funcional

--- lib/reservations/reservations.ex
+++ lib/reservations/reservations.ex
@@ -1,6 +1,6 @@
 defmodule Reservations do
-  alias Reservations.Type
   alias Reservations.Core.ReservationRules
+  alias Reservations.Type
 
   @spec create_restaurant(String.t(), integer()) :: Type.restaurant()
   defdelegate create_restaurant(name, num_tables), to: ReservationRules
